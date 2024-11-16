f232bddd474ba8f63a7b6282b84ac71db12a6171
katoy <youichikato@gmail.com>
2024-11-16 13:18:50 +0900

    Add using node-red on zero

---
+++ raspberry/zero-node-red/README.md
@@ -0,0 +1,14 @@
+
+```
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+
+node-red-start
+
+http://<Raspberry-Pi-IP>:1880
+
+node-red-stop
+
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+
+```
--- raspberry/zero/web-server/app.py
+++ raspberry/zero/web-server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
 from gpiozero import LED, CPUTemperature
 
 # Flask アプリケーションの初期化
@@ -10,31 +10,48 @@ cpu = CPUTemperature()  # CPU 温度を取得するためのオブジェクト
 
 @app.route("/")
 def index():
-    """LED 制御用のホームページを表示"""
-    # 現在のLEDの状態を取得
+    """
+    LED 制御用のホームページを表示
+    """
+    # 現在の LED 状態と CPU 温度を取得
     led_state = "ON" if led.is_lit else "OFF"
-    # CPU温度を取得
     cpu_temp = round(cpu.temperature, 2)  # 温度を小数点第 2 位で丸める
     return render_template("index.html", led_state=led_state, cpu_temp=cpu_temp)
 
 @app.route("/led", methods=["POST"])
 def led_control():
-    """LED のオン/オフを制御"""
-    action = request.form.get("action")
-    if action == "on":
-        led.on()
-    elif action == "off":
-        led.off()
-    
-    # 現在の LED 状態と CPU 温度を取得してページを再描画
-    led_state = "ON" if led.is_lit else "OFF"
-    cpu_temp = round(cpu.temperature, 2)
-    return render_template("index.html", led_state=led_state, cpu_temp=cpu_temp)
+    """
+    JSON データを受け取り LED のオン/オフを制御
+    """
+    if request.content_type != "application/json":
+        return jsonify({"error": "Unsupported Media Type"}), 415
+
+    try:
+        data = request.get_json()  # JSON データを取得
+        print("Received JSON:", data)  # デバッグ用
+        action = data.get("action")  # "action" キーを取得
+
+        if action == "on":
+            led.on()
+            print("LED is turned ON")
+        elif action == "off":
+            led.off()
+            print("LED is turned OFF")
+        else:
+            return jsonify({"error": "Invalid action"}), 400
+
+        # 現在の LED 状態を返す
+        led_state = "ON" if led.is_lit else "OFF"
+        return jsonify({"led_state": led_state}), 200
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({"error": "Invalid request"}), 400
 
 if __name__ == "__main__":
     try:
         # Flask アプリケーションを起動
         app.run(host="0.0.0.0", port=5000, debug=True)
     except KeyboardInterrupt:
-        print("サーバーを終了します。")
+        pass
+
 
