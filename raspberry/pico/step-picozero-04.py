c71e4b9b9426d12466d5c7cfa72f4848eb31a74d
katoy <youichikato@gmail.com>
2024-11-19 06:50:01 +0900

    改行の追加

--- raspberry/pico/step-picozero-04.py
+++ raspberry/pico/step-picozero-04.py
@@ -1,9 +1,11 @@
 from picozero import Button
 from time import sleep
 
+# 内部プルアップ抵抗が picozero.Button で自動的に有効になる
 button = Button(16)
 
 while True:
     if button.is_pressed:
         print("Button is pressed")
     sleep(0.1)
+
