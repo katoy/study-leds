32edcfb3ca8423f4b5fc9a0b911d49235d51cbf5
katoy <youichikato@gmail.com>
2025-01-22 06:52:40 +0900

    fix calc for servo

--- raspberry/kepler-video/036-01.py
+++ raspberry/kepler-video/036-01.py
@@ -7,6 +7,6 @@ servo.freq(50)
 
 while True:
     angle = int(input('What angle do you desire? '))
-    writeVal = 6553 / 180 * angle + 1618
+    writeVal = 6553 / 180 * angle + 1638
     servo.duty_u16(int(writeVal))
     utime.sleep(0.5)
